68516f9d41da82aeb9977553cf82ccc49cf66f60
BEKI77 <berekettadiwos00@gmail.com>
2025-08-23 02:37:59 +0300

    added the structure of the block chain and the world state traits

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -7,3 +7,23 @@ use std::time::SystemTime;
 use blake2::{Blake2b, Digest};
 use std::string::String;
 use std::convert::From;
+
+
+#[derive(Debug, Clone)]
+pub struct Blockchain {
+    /// Stores all the blocks which are accepted already within the blockchain
+    pub blocks: Vec<Block>,
+
+    /// Lookup from AccountID (will be a public key later) to Account.
+    pub accounts: HashMap<String, Account>,
+
+    /// Will store transactions which should be added to the chain but aren't yet
+    pending_transactions: Vec<Transaction>
+}
+
+
+pub trait WorldState {
+    fn get_user_ids(&self) -> Vec<String>;
+
+    fn get_account_by_id_mut(&mut self, id: &String) -> Option<&Account>;
+}
